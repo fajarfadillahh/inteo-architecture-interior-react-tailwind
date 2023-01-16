91030fd84b1247db3b0e083138dc3a3ad877dc6d
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-16 23:47:29 +0700

    create Hero.jsx, part of the Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Header from "../components/Header";
+import Hero from "../parts/Homepage/Hero";
 
 export default function Homepage() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
---
+++ src/parts/Homepage/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import HeroImg from "../../assets/images/hero-image.png";
+import HeroImgBtn from "../../assets/images/hero-button.svg";
+
+export default function Hero() {
+  return (
+    <section className="hero pt-32">
+      <div className="hero__wrapper grid gap-16">
+        <div className="hero__container container">
+          <div className="hero__data text-center">
+            <h1 className="hero__title section-title">
+              make your home comfortable <br /> and elegant
+            </h1>
+            <p className="hero__text section-text">
+              We turn your empty house to a lovely home, making the compact
+              spaces with sapce saving furnitures. Making the unique tastes of
+              yours into reality!
+            </p>
+          </div>
+        </div>
+
+        <div className="hero__overlay relative h-[640px]">
+          <button className="hero__overlay-button absolute left-1/2 -top-[60px] w-[120px] -translate-x-1/2">
+            <img src={HeroImgBtn} alt="hero btn" />
+          </button>
+
+          <img
+            src={HeroImg}
+            alt="hero img"
+            className="hero__overlay-img h-full w-full object-cover object-right"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
