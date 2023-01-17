7b633c508b81ba453d0f0fe21b170f6497f90db4
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-17 20:40:40 +0700

    responsive Awards

--- src/parts/Homepage/Awards.jsx
+++ src/parts/Homepage/Awards.jsx
@@ -10,12 +10,12 @@ export default function Awards() {
     <section className="awards pb-16">
       <div className="awards__wrapper bg-black py-12">
         <div className="awards__container container">
-          <div className="awards__group grid grid-cols-2 gap-12">
-            <div className="awards__content grid gap-4">
+          <div className="awards__group grid grid-cols-2 gap-12 sm:justify-items-center lg:grid-cols-4">
+            <div className="awards__content grid gap-4 md:grid-cols-[repeat(2,max-content)] md:items-center">
               <img
                 src={AwardsImg1}
                 alt="awards icon"
-                className="awards__content-icon w-16"
+                className="awards__content-icon w-16 sm:w-20"
               />
               <h3 className="awards__content-text text-[15px] font-medium text-white">
                 German Design
@@ -24,11 +24,11 @@ export default function Awards() {
               </h3>
             </div>
 
-            <div className="awards__content grid gap-4">
+            <div className="awards__content grid gap-4 md:grid-cols-[repeat(2,max-content)] md:items-center">
               <img
                 src={AwardsImg2}
                 alt="awards icon"
-                className="awards__content-icon w-16"
+                className="awards__content-icon w-16 sm:w-20"
               />
               <h3 className="awards__content-text text-[15px] font-medium text-white">
                 Gold A' Design
@@ -37,11 +37,11 @@ export default function Awards() {
               </h3>
             </div>
 
-            <div className="awards__content grid gap-4">
+            <div className="awards__content grid gap-4 md:grid-cols-[repeat(2,max-content)] md:items-center">
               <img
                 src={AwardsImg3}
                 alt="awards icon"
-                className="awards__content-icon w-16"
+                className="awards__content-icon w-16 sm:w-20"
               />
               <h3 className="awards__content-text text-[15px] font-medium text-white">
                 IF2M Design
@@ -50,11 +50,11 @@ export default function Awards() {
               </h3>
             </div>
 
-            <div className="awards__content grid gap-4">
+            <div className="awards__content grid gap-4 md:grid-cols-[repeat(2,max-content)] md:items-center">
               <img
                 src={AwardsImg4}
                 alt="awards icon"
-                className="awards__content-icon w-16"
+                className="awards__content-icon w-16 sm:w-20"
               />
               <h3 className="awards__content-text text-[15px] font-medium text-white">
                 Good Design
