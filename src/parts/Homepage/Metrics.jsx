1e43376934f3cb11e13fbc24a832b0d8bf79c4f1
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-17 20:53:56 +0700

    responsive Metrics

--- src/parts/Homepage/Metrics.jsx
+++ src/parts/Homepage/Metrics.jsx
@@ -4,9 +4,9 @@ export default function Metrics() {
   return (
     <section className="metrics pb-16">
       <div className="metrics__container container">
-        <div className="metrics__group grid justify-center gap-8">
+        <div className="metrics__group grid justify-center gap-8 md:grid-cols-[repeat(3,max-content)] md:items-center lg:gap-20">
           <div className="metrics__content inline-flex items-center gap-4">
-            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black">
+            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black lg:text-[58px]">
               100%
             </h3>
             <p className="metrics__content-text uppercase text-gray-600">
@@ -16,7 +16,7 @@ export default function Metrics() {
           </div>
 
           <div className="metrics__content inline-flex items-center gap-4">
-            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black">
+            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black lg:text-[58px]">
               299+
             </h3>
             <p className="metrics__content-text uppercase text-gray-600">
@@ -26,7 +26,7 @@ export default function Metrics() {
           </div>
 
           <div className="metrics__content inline-flex items-center gap-4">
-            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black">
+            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black lg:text-[58px]">
               295+
             </h3>
             <p className="metrics__content-text uppercase text-gray-600">
