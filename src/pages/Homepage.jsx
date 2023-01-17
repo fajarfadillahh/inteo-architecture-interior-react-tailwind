f03a8855b6825ec6193268fd822d2a8515bc69c6
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-17 10:38:44 +0700

    create Metrics.jsx, part of the Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Hero from "../parts/Homepage/Hero";
 import Awards from "../parts/Homepage/Awards";
 import About from "../parts/Homepage/About";
+import Metrics from "../parts/Homepage/Metrics";
 
 export default function Homepage() {
   return (
@@ -12,6 +13,7 @@ export default function Homepage() {
       <Hero />
       <Awards />
       <About />
+      <Metrics />
     </>
   );
 }
---
+++ src/parts/Homepage/Metrics.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Metrics() {
+  return (
+    <section className="metrics pb-16">
+      <div className="metrics__container container">
+        <div className="metrics__group grid justify-center gap-8">
+          <div className="metrics__content inline-flex items-center gap-4">
+            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black">
+              100%
+            </h3>
+            <p className="metrics__content-text text-[15px] uppercase text-gray-600">
+              Satisfition <br />
+              cliens
+            </p>
+          </div>
+
+          <div className="metrics__content inline-flex items-center gap-4">
+            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black">
+              299+
+            </h3>
+            <p className="metrics__content-text text-[15px] uppercase text-gray-600">
+              Employess on <br />
+              world
+            </p>
+          </div>
+
+          <div className="metrics__content inline-flex items-center gap-4">
+            <h3 className="metrics__content-number font-serif text-[48px] font-semibold text-black">
+              295+
+            </h3>
+            <p className="metrics__content-text text-[15px] uppercase text-gray-600">
+              Projects <br />
+              completes
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
