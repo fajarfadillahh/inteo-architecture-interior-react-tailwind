5ac271f029e17031d20f273dc2fe1559f4fad315
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-17 15:26:31 +0700

    tracked Footer file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="footer pt-32 pb-8">
+      <div className="footer__container container grid gap-24">
+        <div className="footer__group grid gap-12">
+          <div className="footer__data">
+            <h1 className="footer__data-title section-title">
+              Kick start your dreams home with us
+            </h1>
+            <Link
+              to="/"
+              className="footer__data-button font-serif text-[32px] font-medium italic text-brown-600 underline"
+            >
+              Send us a Hi
+            </Link>
+          </div>
+
+          <div className="footer__wrapper grid gap-6">
+            <div className="footer__wrapper-data">
+              <h1 className="footer__wrapper-title font-serif text-[24px] font-medium text-black">
+                Brooklyn, New York
+              </h1>
+              <p className="footer__wrapper-text section-text pb-0">
+                962 Fifth Avenue Str, 3rd Floor-Trump <br />
+                Building NY 10006, United State.
+              </p>
+            </div>
+
+            <div className="footer__wrapper-data">
+              <p className="footer__wrapper-text section-text pb-0">
+                Email us at:
+              </p>
+              <h1 className="footer__wrapper-title font-serif text-[24px] font-medium text-black">
+                hi@inteointerior.com
+              </h1>
+            </div>
+
+            <div className="footer__wrapper-data">
+              <p className="footer__wrapper-text section-text pb-0">
+                If you're hurry, quick call for us
+              </p>
+              <h1 className="footer__wrapper-title font-serif text-[24px] font-medium text-black">
+                +8 (663) 125-08-59
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer__copy border-t border-gray-300 pt-8 text-center text-[14px] text-black">
+          &copy; 2022 Inteo Interior - Award winning studio. Made with love by
+          Fajar Fadillah A
+        </div>
+      </div>
+    </footer>
+  );
+}
